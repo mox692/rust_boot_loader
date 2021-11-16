27d6ffec012f21b258ae58b9b9e3a68885a571a8
mox692 <55653825+mox692@users.noreply.github.com>
2021-11-16 07:14:31 +0000

    add comment

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@
 #![no_main]
 #![feature(panic_info_message)]
 #![feature(custom_test_frameworks)]
+// MEMO: ここのcrate keywordは、root crateからの相対pathを示す
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
@@ -23,6 +24,7 @@ pub extern "C" fn _start() -> ! {
 
     // Test実行時(cargo test ...)のみ、test_main()の先の
     // instructionが出力される.
+    // Ref: https://doc.rust-jp.rs/rust-by-example-ja/attribute/cfg.html
     #[cfg(test)]
     test_main();
 
@@ -40,12 +42,6 @@ fn panic(info: &PanicInfo) -> ! {
 }
 
 #[cfg(test)]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    blog_os::test_panic_handler(info)
-}
-
-#[cfg(test)]
 fn test_runner(tests: &[&dyn Testable]) {
     println!("Running {} tests", tests.len());
     for test in tests {
@@ -54,7 +50,24 @@ fn test_runner(tests: &[&dyn Testable]) {
     exit_qemu(QemuExitCode::Success);
 }
 
+// x86_64クレートのPort:writeのラップ.
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    blog_os::test_panic_handler(info)
+}
+
 pub trait Testable {
+    // TODO: 調べる.
     fn run(&self) -> ();
 }
 impl<T> Testable for T
@@ -73,18 +86,10 @@ fn trivial_assertion() {
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
+// MEMO: アライメントの指定
+// https://doc.rust-jp.rs/rust-nomicon-ja/repr-rust.html
 #[repr(u32)]
 pub enum QemuExitCode {
     Success = 0x10,
     Failed = 0x11,
-}
-
-// x86_64クレートのPort:writeのラップ.
-pub fn exit_qemu(exit_code: QemuExitCode) {
-    use x86_64::instructions::port::Port;
-
-    unsafe {
-        let mut port = Port::new(0xf4);
-        port.write(exit_code as u32);
-    }
 }
